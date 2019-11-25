475e4fab372d1b83c2436b4ff0bd21874daed2e4
Jayaramanan Kumar <jayaraman@asahitechnologies.com>
2019-11-25 18:44:54 +0530

    added explore widget

--- lib/explore/explore.dart
+++ lib/explore/explore.dart
@@ -18,12 +18,14 @@ class _ExplorePageState extends State<ExplorePage> {
     return Container(
       padding: EdgeInsets.only(left: 20, top: 20),
       child: Column(
-          children: <Widget>[
-            searchBox(),
-            OptionScroll()
-          ],
-
-      ) ,
+        children: <Widget>[
+          searchBox(),
+          SizedBox(height: 10),
+          OptionScroll(),
+          SizedBox(height: 10),
+          placeToStay()
+        ],
+      ),
     );
   }
 
@@ -33,9 +35,36 @@ class _ExplorePageState extends State<ExplorePage> {
         children: <Widget>[
           Text('What can we help you find, Jayaramanan ?',
               style: TextStyle(fontSize: 20)),
-
         ],
       ),
+    );
+  }
+
+  Widget placeToStay() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Airbnb Plus places to stay',
+          style: TextStyle(
+            fontSize: 25,
+            color: Color(0xff484848),
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+        Text('A selection of places to stay verified for quality and design'),
+
+        // Stack(
+        //   children: <Widget>[
+        //     Image(
+        //       fit: BoxFit.contain,
+        //       alignment: Alignment.topCenter,
+        //       image: NetworkImage(
+        //           "https://a0.muscache.com/4ea/air/v2/pictures/fe0ff5f7-41fe-42f4-83d0-60a51549fc45.jpg?t=c:w800-h600,r:w800-h600-sfit,e:fjpg-c75"),
+        //     )
+        //   ],
+        // )
+      ],
     );
   }
 
--- lib/explore/optionScroll.dart
+++ lib/explore/optionScroll.dart
@@ -5,62 +5,87 @@ class OptionScroll extends StatelessWidget {
   final List<BnbOptions> optionList = [
     BnbOptions(
       imageUrl:
-          "https://a0.muscache.com/im/pictures/7517954/21419416_original.jpg?aki_policy=large",
-      label: "hotels",
+          "https://a0.muscache.com/im/pictures/8b7519ec-2c82-4c09-8233-fd4d2715bbf9.jpg?aki_policy=large",
+      label: "Stays",
     ),
     BnbOptions(
       imageUrl:
-          "https://a0.muscache.com/im/pictures/7517954/21419416_original.jpg?aki_policy=large",
-      label: "hotels",
+          "https://a0.muscache.com/im/pictures/a77ee672-e790-4362-8cc5-52bec1371ece.jpg?aki_policy=large",
+      label: "Experiences",
     ),
     BnbOptions(
       imageUrl:
-          "https://a0.muscache.com/im/pictures/7517954/21419416_original.jpg?aki_policy=large",
-      label: "hotels",
+          "https://a0.muscache.com/im/pictures/38f2bfd6-1f4d-46d7-babf-61657deef302.jpg?aki_policy=large",
+      label: "Adventures",
+    ),
+    BnbOptions(
+      imageUrl:
+          "https://a0.muscache.com/im/pictures/da2d8e97-90b7-409f-94ac-5ab0327c289b.jpg?aki_policy=large",
+      label: "Restaurants",
     ),
   ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 5.0),
-        child: Wrap(
-          spacing: 25,
-          children: <Widget>[
-            Align(
-              child: Text(
-                'What can we help you find, Jayaramanan?',
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              alignment: Alignment.topLeft,
-            ),
-            Container(
-              height: 150.0,
-              margin: EdgeInsets.only(top: 25),
-              //padding: EdgeInsets.only(top: 25, bottom: 25),
-              child: ListView.builder(
-                itemCount: optionList.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, position) {
-                  return optionCard(optionList[position], context);
-                },
-              ),
-            )
-          ],
-        ));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 15),
+          child: Text(
+            "What can we help you find, Jayaramanan?",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Color(0xff4a545a)),
+          ),
+        ),
+        SizedBox(
+          height: 150,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: optionList
+                .map((option) => optionCard(option, context))
+                .toList(),
+          ),
+        ),
+      ],
+    );
   }
 
   Widget optionCard(BnbOptions option, BuildContext context) {
     return Container(
-        width: 150,
-        margin: EdgeInsets.symmetric(horizontal: 5),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20.0),
-          color: Colors.white,
-        ),
+      margin: EdgeInsets.only(right: 5, bottom: 5, top: 5),
+      child: Material(
+        elevation: 4.0,
+        borderRadius: BorderRadius.circular(15),
+        shadowColor: Colors.grey[300],
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[faddingImage(option.imageUrl), Text(option.label)],
-        ),    
+          children: <Widget>[
+            Container(
+              height: 110,
+              width: 150,
+              //padding: EdgeInsets.all(5),
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+                child: Image(
+                  fit: BoxFit.contain,
+                  alignment: Alignment.topCenter,
+                  image: NetworkImage(option.imageUrl),
+                ),
+              ),
+            ),
+            Container(
+              height: 30,
+              width: 150,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Text(
+                option.label,
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
